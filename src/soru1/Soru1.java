f92f916c910a09ada8c6a06f4a1334c38669012b
melihcan öztürk <melihcannozturkk@gmail.com>
2022-11-28 16:50:46 +0300

    maraton2

--- src/soru1/Soru1.java
+++ src/soru1/Soru1.java
@@ -2,51 +2,51 @@ package soru1;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-
-
 
 public class Soru1 {
+    static Map<String,Long> kisiler = new HashMap<>();
+    public static void main(String[] args) {
 
-	static Set<Kisi> kisiler = new HashSet<>();
 
-	public static void main(String[] args) {
+        try(Scanner scan = new Scanner(new FileReader("rehber.txt"))){
+            while(scan.hasNextLine()) {
+                String okunanSatir = scan.nextLine();
+                System.out.println(okunanSatir);
+                String [] arr= okunanSatir.split(";");
+                kisiler.put(arr[0],Long.parseLong(arr[2].trim()));
+            }
 
-		
-		try (Scanner scan = new Scanner(new FileReader("rehber.txt"))) {
-			while (scan.hasNextLine()) {
-				String okunanSatir = scan.nextLine();
-				System.out.println(okunanSatir);
-				String[] arr = okunanSatir.split(";");
-				Kisi kisi = new Kisi(arr[0], arr[1], Long.parseLong(arr[2].trim()));
-				kisiler.add(kisi);
-			}
+        } catch (FileNotFoundException e) {
+            System.out.println("Dosya bulunamadı hatası yakalandı.");
+        }
+        System.out.println(kisiler);
 
-		} catch (FileNotFoundException e) {
-			System.out.println("Dosya bulunamadı hatası...");
-		}
-		System.out.println(kisiler);
+        kisiArama();
 
-	}
+    }
+    public static void kisiArama() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen telefonunu aradğınız kişinin adını giriniz:");
+        String isim= scan.nextLine();
 
-	public static void arama() {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Lütfen telefonunu aradğınız kişinin adını giriniz:");
-		String isim = scan.nextLine();
+        System.out.println("Lütfen telefonunu aradğınız kişinin soyadını giriniz:");
+        String soyad= scan.nextLine();
 
-		System.out.println("Lütfen telefonunu aradğınız kişinin soyadını giriniz:");
-		String soyad = scan.nextLine();
+        String adSoyad = isim+" "+soyad;
+        System.out.println(adSoyad);
 
-		String adSoyad = isim + " " + soyad;
-		if (kisiler.contains(adSoyad)) {
-			System.out.println(adSoyad + " isimli kişinin telefon numarası: ");
-		}
+        if(kisiler.containsKey(adSoyad)) {
+            System.out.println(adSoyad+" isimli kişinin telefon numarası: "+kisiler.get(adSoyad));
+        } else {
+            System.out.println("Böyle bir kişi yoktur.");
+        }
 
-	}
-	
-	
+
+
+    }
 
 
 }
